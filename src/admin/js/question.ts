88fcd6d76bff55499e66a25915a2b293fa4b0138
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-08-08 16:55:41 +0100

    [COMMON] Add question winners handling to the admin interface

--- src/admin/js/question.ts
+++ src/admin/js/question.ts
@@ -1,4 +1,5 @@
 import { BlindTestQuestion, parse } from 'ani-grenoble-games-format/dist/QuestionTypes'
+import { GameState, QuestionWinners } from 'ani-grenoble-games-format/dist/GameState'
 import { ipcRenderer } from 'electron';
 
 export function onHintClick(id: string) {
@@ -11,7 +12,7 @@ ipcRenderer.on('question-data', (_, q) => {
     const question: BlindTestQuestion = q;
 
     const xhttp = new XMLHttpRequest();
-    xhttp.onloadend = function() {
+    xhttp.onloadend = function () {
         document.getElementById('question-div')!.innerHTML = this.responseText;
         document.getElementById('answer')!.textContent = question.answer;
         (document.getElementById('question-score') as HTMLInputElement).value = question.points.toString();
@@ -24,7 +25,45 @@ ipcRenderer.on('question-data', (_, q) => {
             button.setAttribute('onclick', `onHintClick(this.id);`);
             document.getElementById('hints')!.appendChild(clone);
         }
+        ipcRenderer.send('admin_question_ready');
     }
     xhttp.open("GET", "/html/question.html", true);
     xhttp.send();
+});
+
+var winners: string[] = new Array();
+function submit_winners() {
+    const response: QuestionWinners = {
+        points: Number((document.getElementById('question-score') as HTMLInputElement).value),
+        players: winners,
+    };
+    ipcRenderer.send("admin-send-winners", response);
+}
+
+const button_handler = (name: string, insertedButton: HTMLButtonElement) => {
+    console.log(insertedButton.style.borderStyle);
+    if (winners.includes(name)) {
+        winners = winners.filter(x => x != name);
+        insertedButton.style.backgroundColor = "";
+    } else {
+        winners.push(name);
+        insertedButton.style.backgroundColor = "#00ff00";
+    }
+}
+
+ipcRenderer.on('game-state-data', (_, s) => {
+    const state: GameState = s;
+    const template = document.getElementById("winners-selection-template")! as HTMLTemplateElement;
+    const winnersDiv = document.getElementById("winners-selection-div")!;
+    for (var teamIdx = 0; teamIdx < state.players.length; teamIdx++) {
+        const buttonID = `winners-selection-button-${teamIdx}`;
+        const player = state.players[teamIdx];
+        const clone = document.importNode(template.content, true);
+        const button = clone.getElementById("winners-selection-template-name")!;
+        button.id = buttonID;
+        button.textContent = player.name;
+        winnersDiv.appendChild(clone);
+        const insertedButton = document.getElementById(buttonID)! as HTMLButtonElement;
+        insertedButton.onclick = () => button_handler(player.name, insertedButton);
+    }
 });
--- src/app/context.ts
+++ src/app/context.ts
@@ -1,9 +1,40 @@
 import { BlindTestQuestion } from 'ani-grenoble-games-format/dist/QuestionTypes'
-import { GameState } from 'ani-grenoble-games-format/dist/GameState'
+import { GameState, QuestionWinners } from 'ani-grenoble-games-format/dist/GameState'
 import { BrowserWindow, ipcMain, ipcRenderer } from 'electron';
 import path = require("path");
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+class Semaphore {
+    constructor(name: string) {
+        ipcMain.on(name, () => {
+            this.count++;
+        });
+    }
+    async wait() {
+        while (this.count == 0) {
+            await delay(1);
+        }
+    }
+    count = 0;
+};
+
+class Queue<T> {
+    constructor(name: string) {
+        ipcMain.on(name, (_, elem) => {
+            this.elems.push(elem);
+        });
+    }
+    async get(): Promise<T> {
+        while (this.elems.length == 0) {
+            await delay(1);
+        }
+        const elem = this.elems[0];
+        this.elems.splice(1);
+        return elem;
+    }
+    elems = new Array<T>();
+};
 
 export class context {
     constructor(user_window: BrowserWindow, admin_window: BrowserWindow) {
@@ -30,11 +61,15 @@ export class context {
         await this.admin_window.loadURL(adminHTML);
         this.user_window.webContents.send('question-data', q);
         this.admin_window.webContents.send('question-data', q);
+        await this.adminQuestionWaiter.wait();
         this.user_window.webContents.send('game-state-data', this.state);
         this.admin_window.webContents.send('game-state-data', this.state);
+        const winners = await this.winnersQueue.get();
     }
     user_window: BrowserWindow;
     admin_window: BrowserWindow;
     state: GameState;
+    adminQuestionWaiter = new Semaphore("admin_question_ready");
     give_hint_listener: (event: any, hint: string) => void;
+    winnersQueue = new Queue<QuestionWinners>("admin-send-winners");
 }
