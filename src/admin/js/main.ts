eeeae96265a510e436d13389b3b06f81f265d02f
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-08-11 16:45:30 +0100

    [MENU] Disallow adding empty or duplicate teams

--- src/admin/js/main.ts
+++ src/admin/js/main.ts
@@ -1,31 +1,44 @@
 import { ipcRenderer } from 'electron'
 
 let teamCounter = 0
+const names: string[] = []
+
+export function onAddTeamButtonClick () {
+    const nameField = (document.getElementById('new-team-name')! as HTMLInputElement)
+    const name = nameField.value
+    const teamsDiv = document.getElementById('teams')
+    const id = `team-div-${teamCounter++}`
+
+    const template = document.getElementById('team-template') as HTMLTemplateElement
+    const clone = document.importNode(template.content, true)
+    clone.getElementById('team-template-div')!.id = id
+    clone.getElementById('team-template-name')!.textContent = name
+    clone.getElementById('team-template-remove')!.onclick = () => {
+        teamsDiv!.removeChild(document.getElementById(id)!)
+        ipcRenderer.send('del_player', name)
+        names.splice(names.indexOf(name), 1)
+        onTeamNameFieldChange(nameField.value)
+    }
+    names.push(name)
+    teamsDiv!.appendChild(clone)
+    ipcRenderer.send('add_player', name)
+
+    // Disallow sending the same input twice.
+    const button = document.getElementById('add-team-button')! as HTMLButtonElement
+    button.disabled = true
+    return false
+}
 
 export function loadMain () {
-    document.getElementById('add-team-button')!.onclick = function () {
-        const name = (document.getElementById('new-team-name')! as HTMLInputElement).value
-        const teamsDiv = document.getElementById('teams')
-        const id = `team-div-${teamCounter++}`
-
-        const template = document.getElementById('team-template') as HTMLTemplateElement
-        const clone = document.importNode(template.content, true)
-        clone.getElementById('team-template-div')!.id = id
-        clone.getElementById('team-template-name')!.textContent = name
-        clone.getElementById('team-template-remove')!.onclick = () => {
-            teamsDiv!.removeChild(document.getElementById(id)!)
-            ipcRenderer.send('del_player', name)
-        }
-
-        teamsDiv!.appendChild(clone)
-        ipcRenderer.send('add_player', name)
-
-        return false
-    }
-
     ipcRenderer.send('admin_ready')
 }
 
 export function debug () {
     ipcRenderer.send('main-menu', 'debug')
+}
+
+export function onTeamNameFieldChange (name: string) {
+    const invalid = name.length === 0 || names.includes(name)
+    const button = document.getElementById('add-team-button')! as HTMLButtonElement
+    button.disabled = invalid
 }
