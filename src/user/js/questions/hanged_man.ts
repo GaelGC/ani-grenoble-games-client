916030cfb54cf3a79fb90ee7357f11b42e07c800
Kalyptien <louppinget@gmail.com>
2021-09-04 19:01:19 +0200

    Changement mineur depuis les retours

--- src/user/js/questions/hanged_man.ts
+++ src/user/js/questions/hanged_man.ts
@@ -1,13 +1,25 @@
 import { ipcRenderer } from 'electron'
-import { HangedManQuestion } from '@gaelgc/ani-grenoble-games-format'
+import { HangedManQuestion, GameState } from '@gaelgc/ani-grenoble-games-format'
 
 let word = ''
 let shownWord = ''
+const players: string[] = []
 
 function updateWordDiv () {
     const wordDiv = document.getElementById('word-div')!
     wordDiv.textContent = shownWord
 }
+
+ipcRenderer.on('game-state-data', (_, s: GameState) => {
+    const stock = s.players
+    for (let i = 0; i < stock.length; i++) {
+        players[i] = stock[i].name
+    }
+})
+
+ipcRenderer.on('current-team', (_, t: number) => {
+    document.getElementById('team-div')!.textContent = players[t]
+})
 
 ipcRenderer.on('question-data', (_, q: HangedManQuestion) => {
     word = q.answer
@@ -25,3 +37,26 @@ ipcRenderer.on('letter', (_, letter: string) => {
     updateWordDiv()
     document.getElementById('used-letter-div')!.textContent += letter.toUpperCase()
 })
+
+ipcRenderer.on('answer', async (_, answer: string) => {
+    const hangedDiv = document.getElementById('word-div')!
+    hangedDiv.textContent = ''
+})
+
+async function start () {
+    let answerDiv = document.getElementById('answer')!
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+    while (answerDiv === null) {
+        answerDiv = document.getElementById('answer')!
+        if (answerDiv === null) {
+            await delay(50)
+        }
+    }
+    answerDiv.style.fontSize = '1.5em'
+    answerDiv.style.fontFamily = 'monospace'
+    answerDiv.style.letterSpacing = '5px'
+    answerDiv.textContent = ''
+}
+
+start()
