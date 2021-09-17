b9fcf1a15842277c7a270bebd90064503b096c3b
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-09-17 22:39:16 +0100

    Final fixes for the JAF
    Auto-detect quote questions
    Translate leaderboard to french

--- src/user/js/questions/quote.ts
+++ src/user/js/questions/quote.ts
@@ -4,4 +4,7 @@ import { QuoteQuestion } from '@gaelgc/ani-grenoble-games-format'
 ipcRenderer.on('question-data', (_, q: unknown) => {
     const question = <QuoteQuestion>q
     document.getElementById('quote-div')!.textContent = question.text
+    if (question.text[0].codePointAt(0) !== 8220) {
+        document.getElementById('titre')!.textContent = 'Question'
+    }
 })
