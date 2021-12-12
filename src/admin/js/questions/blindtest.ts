af193373d84b3d027eabb7d415ad859bf687af1d
Gael GC <83537366+GaelGC@users.noreply.github.com>
2021-12-12 18:06:26 +0000

    [BLINDTEST] Start audio at a random point

--- src/admin/js/questions/blindtest.ts
+++ src/admin/js/questions/blindtest.ts
@@ -4,7 +4,15 @@ import { BlindTestQuestion } from '@gaelgc/ani-grenoble-games-format'
 ipcRenderer.on('question-data', (_, q) => {
     const question: BlindTestQuestion = q
     const template: HTMLTemplateElement = document.getElementById('template') as HTMLTemplateElement
-    const clone = document.importNode(template.content, true)
-    clone.getElementById('audio')!.setAttribute('src', question.path)
+    const clone = document.importNode(template.content, true);
+    (<HTMLAudioElement>clone.getElementById('audio')).src = question.path
+
     document.getElementById('blindtest-div')!.appendChild(clone)
+
+    const audio = <HTMLAudioElement>document.getElementById('audio')
+    audio.addEventListener('loadeddata', () => {
+        let maxStartPoint = audio.duration - 20
+        maxStartPoint = Math.max(0, maxStartPoint)
+        audio.currentTime = Math.random() * maxStartPoint
+    })
 })
