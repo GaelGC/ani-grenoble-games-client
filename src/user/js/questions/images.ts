b1c64a6ca72c81b8337ba67ebd175c0b5bab683e
breviert <theo.breviere@etu.univ-grenoble-alpes.fr>
2024-02-20 16:22:43 +0100

    [FRONT][UI] Image display fixes

--- src/user/js/questions/images.ts
+++ src/user/js/questions/images.ts
@@ -1,5 +1,7 @@
 import { ipcRenderer } from 'electron'
 
+let imageColumn = 0
+
 ipcRenderer.on('show-image', (_, image: string) => {
     const imagesDiv = document.getElementById('images-div')!
     const template = document.getElementById('image-template') as HTMLTemplateElement
@@ -10,26 +12,24 @@ ipcRenderer.on('show-image', (_, image: string) => {
 
     const size = imagesDiv.children.length - 1
 
+    if (size % 2 !== 0) {
+        imageColumn++
+    }
+
     if (size <= 1) {
         imagesDiv.style.gridTemplateColumns = '1fr'
+        resizeImg(imagesDiv.children, 50)
     } else if (size > 1 && size <= 4) {
         imagesDiv.style.gridTemplateColumns = '1fr 1fr'
-    } else if (size > 4 && size <= 6) {
-        imagesDiv.style.gridTemplateColumns = '1fr 1fr 1fr'
-        resizeImg(imagesDiv.children, 20)
-    } else if (size > 6 && size <= 8) {
-        imagesDiv.style.gridTemplateColumns = '1fr 1fr 1fr 1fr'
-        resizeImg(imagesDiv.children, 12)
-    } else if (size > 8 && size <= 10) {
-        imagesDiv.style.gridTemplateColumns = '1fr 1fr 1fr 1fr 1fr'
-        resizeImg(imagesDiv.children, 7)
+        resizeImg(imagesDiv.children, 30)
     } else {
-        imagesDiv.style.gridTemplateColumns = '1fr'
+        imagesDiv.style.gridTemplateColumns = ('1fr ').repeat(imageColumn)
+        resizeImg(imagesDiv.children, 40 / imageColumn)
     }
 })
 
-function resizeImg(array: HTMLCollection, imgSize: number){
-    for(let i=1; i < array.length; i++){
-        array[i].setAttribute("style", "margin: 0.5em; width: " + imgSize + "vw; height: " + imgSize + "vh")
+function resizeImg (array: HTMLCollection, imgSize: number) {
+    for (let i = 1; i < array.length; i++) {
+        array[i].setAttribute('style', 'margin: 0.5em; width: ' + imgSize + 'vw; height: ' + imgSize + 'vh; object-fit: contain')
     }
 }
