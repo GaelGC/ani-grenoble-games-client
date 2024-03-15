f15251a36a0ae2da73cd954adeee480b37907769
Theo BREVIERE <theo.breviere@etu.univ-grenoble-alpes.fr>
2024-03-15 17:55:52 +0100

    [FRONT] Normalize audio and add function for reverb, pitch up/down and reverse

--- src/admin/js/questions/blindtest.ts
+++ src/admin/js/questions/blindtest.ts
@@ -1,49 +1,168 @@
 import { ProtocolResponse, ipcRenderer } from 'electron'
 import { BlindTestQuestion, GameConfiguration } from '@gaelgc/ani-grenoble-games-format'
-import { log } from 'console'
+
+function audioBufferToBlob (audioBuffer: AudioBuffer, audioType: string) {
+    const numberOfChannels = audioBuffer.numberOfChannels
+    const sampleRate = audioBuffer.sampleRate
+    const length = audioBuffer.length
+    const interleaved = new Float32Array(length * numberOfChannels)
+    for (let channel = 0; channel < numberOfChannels; channel++) {
+        const channelData = audioBuffer.getChannelData(channel)
+        for (let i = 0; i < length; i++) {
+            interleaved[i * numberOfChannels + channel] = channelData[i]
+        }
+    }
+    const dataView = encodeWAV(interleaved, numberOfChannels, sampleRate)
+    const blob = new Blob([dataView], { type: audioType })
+    return blob
+}
+
+function encodeWAV (samples : Float32Array, channels: number, sampleRate: number) {
+    const buffer = new ArrayBuffer(44 + samples.length * 2)
+    const view = new DataView(buffer)
+    writeString(view, 0, 'RIFF')
+    view.setUint32(4, 36 + samples.length * 2, true)
+    writeString(view, 8, 'WAVE')
+    writeString(view, 12, 'fmt ')
+    view.setUint32(16, 16, true)
+    view.setUint16(20, 1, true)
+    view.setUint16(22, channels, true)
+    view.setUint32(24, sampleRate, true)
+    view.setUint32(28, sampleRate * channels * 2, true)
+    view.setUint16(32, channels * 2, true)
+    view.setUint16(34, 16, true)
+    writeString(view, 36, 'data')
+    view.setUint32(40, samples.length * 2, true)
+    floatTo16BitPCM(view, 44, samples)
+    return view
+}
+
+function floatTo16BitPCM (output: DataView, offset: number, input: Float32Array) {
+    for (let i = 0; i < input.length; i++, offset += 2) {
+        const s = Math.max(-1, Math.min(1, input[i]))
+        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true)
+    }
+}
+
+function writeString (view: DataView, offset: number, string: string) {
+    for (let i = 0; i < string.length; i++) {
+        view.setUint8(offset + i, string.charCodeAt(i))
+    }
+}
 
 let config: GameConfiguration
 ipcRenderer.on('question-configuration', (_, c) => {
     config = c
 })
 
-ipcRenderer.on('question-data', (_, q) => {
+ipcRenderer.on('question-data', async (_, q) => {
     const question: BlindTestQuestion = q
     const template: HTMLTemplateElement = document.getElementById('template') as HTMLTemplateElement
     const clone = document.importNode(template.content, true)
 
-    clone.getElementById('audio')!.setAttribute('src', question.path)
+    clone.getElementById('audio')
     document.getElementById('blindtest-div')!.style.backgroundImage = 'url(' + question.answerImage.replace(/ /g, '%20') + ')'
     document.getElementById('blindtest-div')!.style.backgroundSize = 'cover'
     document.getElementById('blindtest-div')!.appendChild(clone)
     document.getElementById('answer')!.textContent = q.answer
 
     const audio = <HTMLAudioElement>document.getElementById('audio')
-    audio.src = question.path
+    // audio.src = question.path
 
-    // Vitesse x2
-    // audio.playbackRate = 2
     const audioCtx = new AudioContext()
-    const source = audioCtx.createMediaElementSource(audio)
 
-    const normalizeAsync = async () => {
-        log('start ' + question.path)
-        const response = await fetch(question.path)
-        log('response')
-        const myArrayBuffer = await audioCtx.decodeAudioData(await response.arrayBuffer())
-        log('arraybuffer')
+    const response = await fetch(question.path)
+    let arrayBuffer = await audioCtx.decodeAudioData(await response.arrayBuffer())
 
-        for (let channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-            const bufferData = myArrayBuffer.getChannelData(channel)
+    const normalizeAsync = async (audioBuffer : AudioBuffer) => {
+        const maxAmplitudeScale = 0.25
+
+        for (let channel = 0; channel < audioBuffer.numberOfChannels; channel++) {
             let maxVolume = 0
+            const bufferData = audioBuffer.getChannelData(channel)
             for (let i = 0; i < bufferData.length; i++) {
                 const volume = Math.abs(bufferData[i])
                 if (volume > maxVolume) {
                     maxVolume = volume
                 }
             }
-            log('maxvol' + maxVolume)
+            for (let i = 0; i < bufferData.length; i++) {
+                bufferData[i] = maxAmplitudeScale * (bufferData[i] / maxVolume)
+            }
         }
     }
-    normalizeAsync()
+    await normalizeAsync(arrayBuffer)
+    // arrayBuffer = createReverse()
+    // arrayBuffer = changePitch(1.33)
+    audio.src = URL.createObjectURL(audioBufferToBlob(arrayBuffer, 'audio/wav'))
+    // createReverb()
+    // createReverbToUnderwater()
+
+    async function createReverb () {
+        const source = audioCtx.createMediaElementSource(audio)
+        const convolver = audioCtx.createConvolver()
+        const modifier = await fetch('question://./Musique/samples_audio_impulse-responses_matrix-reverb2.wav')
+        const reverbBuffer = await modifier.arrayBuffer()
+        convolver.buffer = await audioCtx.decodeAudioData(reverbBuffer)
+
+        source.connect(convolver)
+        convolver.connect(audioCtx.destination)
+    }
+
+    async function createReverbToUnderwater () {
+        const source = audioCtx.createMediaElementSource(audio)
+        const convolver = audioCtx.createConvolver()
+        const modifier = await fetch('question://./Musique/underwater.wav')
+        const reverbBuffer = await modifier.arrayBuffer()
+        convolver.buffer = await audioCtx.decodeAudioData(reverbBuffer)
+
+        source.connect(convolver)
+        convolver.connect(audioCtx.destination)
+    }
+
+    function changePitch (speedValue : number) {
+        const pitchedLength = Math.floor(arrayBuffer.length / speedValue)
+        const pitchedBuffer = new AudioBuffer({
+            sampleRate: arrayBuffer.sampleRate,
+            numberOfChannels: arrayBuffer.numberOfChannels,
+            length: pitchedLength
+        })
+
+        for (let channel = 0; channel < arrayBuffer.numberOfChannels; channel++) {
+            const bufferData = arrayBuffer.getChannelData(channel)
+            const bufferDataCopy = new Float32Array(pitchedLength)
+            for (let i = 0; i < bufferDataCopy.length; i++) {
+                const calc = (i * speedValue) - Math.floor(i * speedValue)
+                bufferDataCopy[i] = (1 - calc) * bufferData[Math.floor(i * speedValue)] + calc * bufferData[Math.floor(i * speedValue) + 1]
+            }
+            pitchedBuffer.copyToChannel(bufferDataCopy, channel)
+        }
+        return pitchedBuffer
+        /* const bufferSource = audioCtx.createBufferSource()
+        bufferSource.buffer = arrayBuffer
+        bufferSource.detune.value = 1000
+        bufferSource.connect(audioCtx.destination)
+        bufferSource.start() */
+        // arrayBuffer = bufferSource.buffer
+    }
+
+    function createReverse () {
+        for (let channel = 0; channel < arrayBuffer.numberOfChannels; channel++) {
+            const bufferData = arrayBuffer.getChannelData(channel)
+            const bufferDataCopy = new Float32Array(bufferData.length)
+            for (let i = 0; i < bufferData.length; i++) {
+                bufferDataCopy[i] = bufferData[bufferData.length - i]
+            }
+            arrayBuffer.copyToChannel(bufferDataCopy, channel)
+        }
+        return arrayBuffer
+    }
+
+    if (config?.randomSample) {
+        audio.addEventListener('loadeddata', () => {
+            let maxStartPoint = audio.duration - 20
+            maxStartPoint = Math.max(0, maxStartPoint)
+            audio.currentTime = Math.random() * maxStartPoint
+        })
+    }
 })
